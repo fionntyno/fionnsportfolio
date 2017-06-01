811e62396a8212e225baf781f5fd7036f5a13c56
fionntyno <15ftynan@stjosephsrush.ie>
2017-06-01 08:40:25 +0100

    calculatorbabby.py
    changed

--- fionnisbae.py
+++ fionnisbae.py
@@ -5,11 +5,11 @@ while True:
  print("subtract = -")
  print("multiply = *")
  print("subtract = /")
- number1=input("type first number")
- number2=input("type second number")
+ number1=input("choose any number and it will be the first number")
+ number2=input("choose a second number")
  print("would you like to add, multiply, subtract or divide")
  question1=input("type question")
- ask=input("would you like a third number")
+ ask=input("do you need or want another number")
 
 
  if ask==("yes"):
@@ -17,10 +17,10 @@ while True:
  r =  ("number1") ("question1") ("number2") ("question2") ("number3")
  print(r)
  elif ask==("no"):
-     print("ok")
+     print("that's too bad ")
    
  else:
-     print("error please try again")
+     print("bro, it has to be yes or no")
 
 
  c = ("number1") ("question1") ("number2")
@@ -35,6 +35,6 @@ while True:
     print(c)
    
  else:
-     print("error please try again")
+     print("that number is invalid")
     
  print("there is your answer")
